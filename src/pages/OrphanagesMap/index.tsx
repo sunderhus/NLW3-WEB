65ad513ad99977c23a9c83cf250f3af03ce8e40b
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-11-06 18:27:31 -0300

    feature: new page of orphanages implemented without map.

--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import happyMarker from '../../assets/images/marker.svg';
+
+import { Page } from './styles';
 const OrphanagesMap: React.FC = () => {
   return (
-    <Container>
-      <p>página de mapa.</p>
-    </Container>
+    <Page>
+      <aside>
+        <header>
+          <img src={happyMarker} alt="happy" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando sua visita.</p>
+        </header>
+        <footer>
+          <strong>Porto Alegre</strong>
+          <span>Rio Grande do Sul</span>
+        </footer>
+      </aside>
+      <div>mapa virá aqui..</div>
+
+      <Link to="/map" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </Page>
   );
 };
 
